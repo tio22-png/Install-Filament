1941cf2540bf1a46a7ea49a0c23853ec16d9745f
tio <hanggoronursetio@gmail.com>
2026-04-15 13:05:59 +0700

    Jobsheet 6

---
+++ app/Filament/Resources/Posts/Schemas/PostForm.php
@@ -0,0 +1,91 @@
+<?php
+
+namespace App\Filament\Resources\Posts\Schemas;
+
+use Filament\Schemas\Schema;
+use Filament\Forms\Components\TextInput;
+use Filament\Forms\Components\Select;
+use Filament\Forms\Components\ColorPicker;
+use Filament\Forms\Components\RichEditor;
+use Filament\Forms\Components\FileUpload;
+use Filament\Forms\Components\TagsInput;
+use Filament\Forms\Components\DateTimePicker;
+use Filament\Forms\Components\Toggle;
+use Filament\Schemas\Components\Section;
+use Filament\Schemas\Components\Group;
+
+class PostForm
+{
+    public static function configure(Schema $schema): Schema
+    {
+        return $schema
+            ->components([
+
+                // 🔥 KIRI (2/3)
+                Group::make([
+                    Section::make('Post Details')
+                        ->description('Fill in the main content')
+                        ->icon('heroicon-o-document-text')
+                        ->schema([
+                            TextInput::make('title')
+                                ->rules('required|min:5|max:10')
+                                ->validationMessages([
+                                    'required' => 'Title wajib diisi',
+                                    'min' => 'Title minimal 5 karakter',
+                                ]),
+
+                            TextInput::make('slug')
+                                ->rules('required|min:3')
+                                ->unique(ignoreRecord: true)
+                                ->validationMessages([
+                                    'unique' => 'Slug harus unik',
+                                    'min' => 'Slug minimal 3 karakter',
+                                ]),
+
+                            Select::make('category_id')
+                                ->relationship('category', 'name')
+                                ->required()
+                                ->validationMessages([
+                                    'required' => 'Category wajib dipilih',
+                                ])
+                                ->preload()
+                                ->searchable(),
+
+                            ColorPicker::make('color'),
+
+                            RichEditor::make('body')
+                                ->columnSpanFull(),
+                        ])
+                        ->columns(2),
+                ])->columnSpan(2),
+
+                // 🔥 KANAN (1/3)
+                Group::make([
+                    Section::make('Meta')
+                        ->icon('heroicon-o-cog')
+                        ->schema([
+                            Toggle::make('published')
+                                ->label('Published'),
+
+                            DateTimePicker::make('published_at'),
+
+                            TagsInput::make('tags'),
+                        ]),
+
+                    Section::make('Thumbnail')
+                        ->icon('heroicon-o-photo')
+                        ->schema([
+                            FileUpload::make('image')
+                                ->required()
+                                ->validationMessages([
+                                    'required' => 'Image wajib diupload',
+                                ])
+                                ->disk('public')
+                                ->directory('posts'),
+                        ]),
+                ])->columnSpan(1),
+
+            ])
+            ->columns(3); 
+    }
+}
---
+++ app/Filament/Resources/Posts/Tables/PostsTable.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Filament\Resources\Posts\Tables;
+
+use Filament\Actions\BulkActionGroup;
+use Filament\Actions\DeleteBulkAction;
+use Filament\Actions\EditAction;
+use Filament\Tables\Table;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Columns\ColorColumn;
+use Filament\Tables\Columns\ImageColumn;
+
+class PostsTable
+{
+    public static function configure(Table $table): Table
+    {
+        return $table
+            ->columns([
+                //
+                TextColumn::make('title'),
+                TextColumn::make('slug'),
+                TextColumn::make('category.name'),
+                ColorColumn::make('color'),
+                ImageColumn::make('image')
+                    ->disk('public'),
+            ])
+            ->filters([
+                //
+            ])
+            ->recordActions([
+                EditAction::make(),
+            ])
+            ->toolbarActions([
+                BulkActionGroup::make([
+                    DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+}
